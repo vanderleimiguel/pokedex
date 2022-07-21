45ec4ec46cda71d884cab98819c1b7fc933b1f89
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-07-20 22:53:05 -0300

    cards funcionando todos os dados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,31 @@
 // ***********************************************************
 //Variaveis gerais
 let page = 1
+let pokeDescrip
 
 // ***********************************************************
-//Função para extrair dados da API
-async function pokedex() {
-  let pokeImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${page}.png`
-
-  let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${page}`)
-  let data = await response.json()
-
-  let response2 = await fetch(
+//Função para extrair descrição da API
+async function pokedex1() {
+  let response1 = await fetch(
     `https://pokeapi.co/api/v2/pokemon-species/${page}`
   )
+  let data1 = await response1.json()
+  pokeDescrip = data1.flavor_text_entries[0].flavor_text
+}
+
+//Função para extrair nome, id, tipo e imagem da API
+async function pokedex2() {
+  //extrai imagem
+  let pokeImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${page}.png`
+
+  //extrai nome, id e tipo
+  let response2 = await fetch(`https://pokeapi.co/api/v2/pokemon/${page}`)
   let data2 = await response2.json()
-  let pokeDescrip = data2.flavor_text_entries[0].flavor_text
+  let pokeName = data2.name
+  let pokeId = data2.id
+  let pokeType = data2.types[0].type.name
 
-  let pokeName = data.name
-  let pokeId = data.id
-  let pokeType = data.types[0].type.name
-
+  //dados que serão inseridos no HTML
   document.querySelector('#cards').insertAdjacentHTML(
     'beforeend',
     `
@@ -33,9 +39,7 @@ async function pokedex() {
               <h2 class="name">${pokeName}</h2>
               <p class="descrip">Nº ${pokeId}</p>
               <h4>Type</h4>
-              <p class="descrip">${pokeType}</p>
-              
-              
+              <p class="descrip">${pokeType}</p>     
           </div>
       </div>
     
@@ -57,7 +61,8 @@ async function pokedex() {
 //chamada da primeira lista na pagina inicial
 let n = 1
 while (n < 13) {
-  pokedex()
+  pokedex1()
+  pokedex2()
   page++
   n++
 }
@@ -66,7 +71,8 @@ while (n < 13) {
 function viewMore() {
   let n2 = 1
   while (n2 < 13) {
-    pokedex()
+    pokedex1()
+    pokedex2()
     page++
     n2++
   }
